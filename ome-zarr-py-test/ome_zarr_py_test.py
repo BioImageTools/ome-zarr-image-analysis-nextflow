ac27eb5ce5c43a1ef419962f10da5241487dd9bd
Joost de Folter <folterj@gmail.com>
2023-11-07 11:22:47 +0100

    Added python ome-zarr-py test script

---
+++ ome-zarr-py-test/ome_zarr_py_test.py
@@ -0,0 +1,15 @@
+from ome_zarr.io import parse_url
+from ome_zarr.reader import Reader
+
+#url = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0062A/6001240.zarr"
+url = "data/xy_8bit__nuclei_PLK1_control.ome.zarr"
+
+# read the image data
+store = parse_url(url, mode="r").store
+reader = Reader(parse_url(url))
+# nodes may include images, labels etc
+nodes = list(reader())
+# first node will be the image pixel data
+image_node = nodes[0]
+metadata = image_node.metadata
+dask_data = image_node.data
