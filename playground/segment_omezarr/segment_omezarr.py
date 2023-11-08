a85596291506d1a7aa8de64ff2e7e5653ed78832
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-11-08 09:44:21 +0100

    Update segment_omezarr.py

--- playground/segment_omezarr/segment_omezarr.py
+++ playground/segment_omezarr/segment_omezarr.py
@@ -6,33 +6,45 @@ import zarr
 from ome_zarr import writer
 import numpy as np
 
-
 def segment(inpath,
             resolution=0,
+            channel = 1,
             export_labels=False,
             outpath=None
             ):
     ### Apply otsu threshold to OME-Zarr and write output to a separate OME-Zarr directory.
     # Read OME-Zarr and specify a resolution layer.
-    reader = Reader(parse_url(inpath))
-    inputs = list(reader())
-    image = inputs[0].data[int(resolution)]
+    r = reader.Reader(parse_url(inpath))
+    inputs = list(r())
+    data = inputs[0].data
+    multimeta = inputs[0].metadata
+    layer = data[int(resolution)]
     # Get the numpy array and perform threshold
-    t = filters.threshold_otsu(image)
-    mask = morphology.label(image > t)
+    axes = multimeta['axes']
+    axorder = ''.join([item['name'] for item in axes])
+    ch_idx = axorder.index('c')
+    # Get the numpy array and perform threshold
+    array = np.array(layer)
+    slicer = [slice(None, None, None) for _ in range(array.ndim)]
+    slicer[ch_idx] = slice(channel, channel + 1, None)
+    sliced = array[tuple(slicer)]
+    t = filters.threshold_otsu(sliced)
+    mask = morphology.label(sliced > t)
+    # resize the top resolution layer to get the lower layers of the pyramid
+    shape = np.array(mask.shape)
+    nres = len(data)
+    new_shapes = [np.where(shape > 1, shape // 2 ** n, shape) for n in range(nres)]
+    layers = [transform.resize(mask, tuple(shape), preserve_range = True).astype(np.uint8) for shape in new_shapes]
     # Save the output
     if export_labels:
         # Write labels as a separate OME-Zarr hierarchy
         assert outpath is not None, "If export_labels is True, outpath must be specified as a directory path."
-        group = zarr.open_group(outpath, mode='a')
-        _ = writer.write_image(image=mask, group=group,
-                               storage_options={'dimension_separator': '/'})
+        gr = zarr.open_group(outpath, mode = 'a')
+        _ = writer.write_multiscale(pyramid = layers, group = gr)
     else:
         # Write labels to the labels subdirectory of the input OME-Zarr hierarchy
-        group = zarr.open_group(inpath, mode='a')
-        _ = writer.write_labels(labels=mask, group=group, name='otsu',
-                                storage_options={'dimension_separator': '/'})
-
+        gr = zarr.open_group(inpath, mode='a')
+        _ = writer.write_multiscale_labels(pyramid = layers, group = gr, name = 'thresholded', storage_options={'dimension_separator': '/'})
 
 if __name__ == '__main__':
     inpath = "data/xy_8bit__nuclei_PLK1_control.ome.zarr"
